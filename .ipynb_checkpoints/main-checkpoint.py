eb140412e5cc4a5adc81a1fe001eb4ae0d5adc42
NikitaShlapak <nikitashlapak04@gmail.com>
2025-05-13 20:45:51 +0000

    commit

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,6 @@
+from runs.depleyion_multy import main
+
+if __name__ == '__main__':
+    start_from = 1
+    output_dir = 'results/depletion/v6'
+    main(start_from=start_from, output_dir=output_dir, skip_prob=0.7)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from runs.depleyion_multy import main
 
 if __name__ == '__main__':
-    start_from = 0
-    output_dir = 'results/depletion/v4'
-    main(start_from=start_from, output_dir=output_dir, skip_prob=0.5)
+    start_from = 15
+    output_dir = 'results/depletion/v6'
+    main(start_from=start_from, output_dir=output_dir, skip_prob=0.7)

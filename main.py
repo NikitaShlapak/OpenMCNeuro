8f8f6268aca7524acbfffef559215581ecbf0f18
nikita <nikitashlapak04@gmail.com>
2025-09-11 15:37:35 +0300

    savedir

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 if __name__ == '__main__':
     start_from = 0
-    output_dir = './depletion/v7'
+    output_dir = 'results/depletion/v7'
     os.makedirs(output_dir, exist_ok=True)
 
     run_all_experiments(start_from=start_from, output_dir=output_dir, skip_prob=0.7)
